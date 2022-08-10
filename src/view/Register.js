f56f4c45d3f34a8adfdb26f17bd0bcd2b40947f0
sulovigo <sulovigo@fit.cvut.cz>
2022-08-10 18:27:42 +1000

    Sign up validation

--- src/view/Register.js
+++ src/view/Register.js
@@ -1,22 +1,65 @@
 import "../css/Register.css"
-function Register() {
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+
+function Register(props) {
+    const navigate = useNavigate();
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+    let err;
+    // validates the password
+    function validate(user, password) {
+        if (user.password !== password){
+            return 'Passwords don\'t match!'
+        }
+
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const user = {
+            name: e.target[0].value,
+            email:  e.target[1].value,
+            password: e.target[2].value
+        }
+
+        //todo: Validation of inputs
+        err = validate(user, e.target[3].value)
+        if (err){
+            console.log(err)
+            let error = document.getElementById('error')
+            error.innerText = err
+        }
+        else {
+            setUser(user)
+            localStorage.setItem('user', JSON.stringify(user))
+            navigate('/')
+        }
+    }
+
     return (
         <div className={`row justify-content-center`}>
-            <form className={`col-4 mt-5 pt-3 form-rectangle rounded-3 shadow p-3`}>
-                <div className="mb-4">
-                    <input type="text" placeholder="Full name" className="form-control " id="inputName" aria-describedby="nameHelp"/>
+            <form className={`col-4 mt-5 pt-3 form-rectangle rounded-3 shadow p-3`} onSubmit={e => handleSubmit(e)}>
+                <div className="mb-3">
+                    <input type="text" placeholder="Full name" className="form-control " id="inputName" aria-describedby="nameHelp" required/>
                 </div>
                 <div className="mb-3">
                     <input type="email" placeholder="Email address" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
-                    <input type="email" placeholder="Password" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    <div id="passwordHelp" className="form-text">Password restrictions.</div>
+                    <input type="password" placeholder="Password" className="form-control" id="examplePassword" aria-describedby="passwordHelp" required/>
+                    <div id="passwordHelp" className="form-text">Must contain at least one number, uppercase and lowercase letter, and more than 7 characters.</div>
+                </div>
+                <div className="mb-3">
+                    <input type="password" placeholder="Confirm password" className="form-control" id="examplePasswordConfirm" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" aria-describedby="passwordConfirmHelp" required/>
                 </div>
                 <div className="mb-3">
                     <button className={`btn btn-outline-primary`} type="submit"> Sign up! </button>
                 </div>
+                <div className="mb-3" id={`error`}>  </div>
             </form>
         </div>
     );
