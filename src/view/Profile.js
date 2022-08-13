f821ccc198db97e092df80b7df8db0672a8569fb
sulovigo <sulovigo@fit.cvut.cz>
2022-08-13 18:15:51 +1000

    Updated profile

--- src/view/Profile.js
+++ src/view/Profile.js
@@ -2,6 +2,8 @@ import "../css/Profile.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faTrashCan} from "@fortawesome/free-solid-svg-icons";
 function Profile(props){
+    const date = new Date(props.user.joined)
+    console.log(date.getDate())
     return(
         <div className={`row justify-content-center mt-5`}>
             <div className={`col-6 profile pt-3 pb-3 rounded-3 shadow p-3`}>
@@ -9,17 +11,18 @@ function Profile(props){
                     <div className={`col-3 profile-pic`}>
                         <img className={`rounded-5`} src={require('../assets/avatar.jpg')} alt={`avatar`}/>
                     </div>
-                    <div className={`col-6 profile-info d-flex`}>
-
+                    <div className={`col-6 profile-info d-flex flex-column`}>
+                        <span className={`name mb-3`}> {props.user.name} </span>
+                        <span className={`email`}> {props.user.email} </span>
                     </div>
                     <div className={`col-3 actions d-flex flex-column justify-content-center`}>
                         <button type="button" className="btn btn-primary"><FontAwesomeIcon icon={faPencil}/> Edit </button>
-                        <button type="button" className="btn btn-primary mt-2"><FontAwesomeIcon icon={faTrashCan} /> Delete </button>
+                        <button type="button" className="btn btn-primary mt-3"><FontAwesomeIcon icon={faTrashCan} /> Delete </button>
                     </div>
                 </div>
                 <hr className={`solid`}/>
                 <div className={`row`}>
-                    <span className={`col-3 justify-content-lg-start`}> Joined: 29th July</span>
+                    <span className={`col-5 justify-content-lg-start`}> Joined: {date.getDate()}/{date.getMonth()}/{date.getFullYear()}</span>
                 </div>
             </div>
         </div>
